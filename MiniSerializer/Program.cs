a4e170b2259aee8645e5da76c589eadeb4612ad8
Kelvin Nose <kelvinjosse@gmail.com>
2016-04-01 15:46:11 -0600

    App Folder for Main Testing (not needed)

---
+++ MiniSerializer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Serializer.Logic;
+
+namespace MiniSerializer
+{
+    
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //TODO app implementation
+        }
+    }
+}
